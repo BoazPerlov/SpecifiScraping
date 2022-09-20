d8a3c75eca28c75d535a74db9573e4854374a9d8
Boaz Perlov <boaz@perlov.co.uk>
2022-09-20 09:18:24 +0100

    First commit

---
+++ Leisuretech Scraping/leisuretecScraper.py
@@ -0,0 +1,60 @@
+
+from bs4 import BeautifulSoup as bs
+import pandas as pd
+import requests
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import re
+from tqdm import tqdm
+
+categoryList = ['sound','lighting','special-effects','ancillary', 'video']
+originalUrl = 'https://www.leisuretec.co.uk/category/'
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'}
+
+dfLeisureTec = pd.DataFrame()
+
+for category in categoryList:
+    shopUrl = originalUrl + category
+    shopHtml = requests.get(shopUrl)
+    bSoup = bs(shopHtml.text,'lxml')
+    subCategories = bSoup.find_all('a',{'class':'listing-link'})
+    for subCat in tqdm(subCategories):
+        subCatHtml = requests.get('https://www.leisuretec.co.uk' + subCat['href'])
+        subCatSoup = bs(subCatHtml.text,'lxml')
+        subSubCategories = subCatSoup.find_all('a',{'class':'listing-link'})
+        try:
+            for subSubCat in subSubCategories:
+                subSubHtml = requests.get('https://www.leisuretec.co.uk' + subSubCat['href'])
+                subSubSoup = bs(subSubHtml.text,'lxml')
+                products = subSubSoup.find_all('a',{'class':'product-page-link'})
+                for product in products:
+                    productHtml = requests.get(('https://www.leisuretec.co.uk' + product['href']))
+                    productSoup = bs(productHtml.text,'lxml')
+                    image = 'https://www.leisuretec.co.uk' + productSoup.find('img',{'id':'main-img'})['src']
+                    rrp = productSoup.find('div',{'class':'product-page-price'}).get_text()
+                    brands = productSoup.find_all('dt')
+                    for brand in brands:
+                        if 'Brand' in brand.get_text():
+                            productBrand = brand.find_next('dd').get_text()
+                        if 'Code' in brand.get_text():
+                            mpn = brand.find_next('dd').get_text()
+                    nameDivs = productSoup.find_all('div', {'class':'col-xs-12'})
+                    for nameDiv in nameDivs:
+                        if nameDiv.findChild('h1'):
+                            productName = nameDiv.findChild('h1').get_text()
+                    descDiv = productSoup.find('div', {'class':'product-page-information-bulletpoints-container'}).findChild('ul')
+                    description = descDiv.get_text()
+                    #specDiv = productSoup.find('div', {'class':'product-page-tabs-tab-pane-container'}).findChild('ul')
+                    #specs = specDiv.get_text()
+                    #manualLink = productSoup.find('a',{'class':'product-page-information-attachments-list-item-link'})['href']
+                    #print(productSoup.find_all('li',{'class':'product-page-tabs-list-item'})['href'])
+                    scrapDict = {'Link':'https://www.leisuretec.co.uk' + product['href'], 'brand':productBrand, 'productName':productName,'RRP':rrp, 'mpn':mpn, 'Description':description,'image':image}
+                    dfLeisureTec = dfLeisureTec.append(scrapDict, ignore_index=True)
+                    dfLeisureTec.to_excel('LeisureTec.xlsx',index=False)
+                
+        except:
+            continue
+dfLeisureTec.to_excel('LeisureTec.xlsx',index=False)
+
---
+++ Midwich Scraping/MidwichScraper.py
@@ -0,0 +1,89 @@
+from logging import exception
+from bs4 import BeautifulSoup as bs
+import pandas as pd
+import requests
+from urllib.parse import urljoin
+import urllib
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import re
+from tqdm import tqdm
+
+letterList = ['abc', 'def','ghi','jkl','mno','pqr','stu','vwx','yz']
+shopUrl = 'https://www.midwich.com/technologies/manufacturers/'
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'}
+
+dfMidwich = pd.DataFrame()
+
+shopHtml = requests.get(shopUrl)
+bSoup = bs(shopHtml.text,'lxml')
+brandList = bSoup.find_all("div", {"class": "brands__list"})
+for blist in brandList:
+    brands = blist.findChildren('a')
+    for brand in tqdm(brands):
+        if 'brand' in brand['href']:
+            brandHtml = requests.get(brand['href'])
+            brandName = brand['href'].split('/')[-1]
+            brandSoup = bs(brandHtml.text,'lxml')
+            paging = brandSoup.find('ul', {'class':'pagination'})
+            if paging:
+                for i in range(30):
+                    brandHtml = requests.get(brand['href'] + '/' +str(i))
+                    brandSoup = bs(brandHtml.text,'lxml')
+                    productList = brandSoup.find_all("a", {"class": "tile-compare"})
+                    for product in productList:
+                        productHtml = requests.get('https://store.midwich.com' + product['href'])
+                        productSoup = bs(productHtml.text,'lxml')
+                        productName = product['href'].split('/')[3]
+                        rrp = productSoup.find('p', {'class':'rrp'}).get_text().strip().replace('RRP:','')
+                        divList = productSoup.find_all('div', {'class':'col-md-7'})
+                        for div in divList:
+                            divC = div.findChildren('strong')
+                            mpn = divC[0].get_text().replace('N/A',"")
+                        try:
+                            description1 = productSoup.find('div', {'class':'midwTruncate'}).findChild('h4').get_text()
+                            description2 = productSoup.find('div', {'class':'midwTruncate'}).findChild('p').get_text()
+                            fullDescription = description1 + '\n' + description2
+                            image = 'https://store.midwich.com' + productSoup.find('img',{'class':'product-image HD1080'})['src']
+                        except:
+                            continue
+                        features = productSoup.find_all('td',{'class':'feature'})
+                        for feature in features:
+                            if 'Dimensions' in feature.get_text():
+                                dimensions = feature.find_next('td').text.strip()
+                            if 'Weight' in feature.get_text():
+                                weight = (feature.find_next('td').text.strip().replace('kg',""))
+                        dfMidwich = dfMidwich.append({'Link':'https://store.midwich.com' + product['href'], 'brand':brandName, 'productName':productName,'RRP':rrp, 'mpn':mpn, 'Description':fullDescription,'image':image, 'dimensions':dimensions,'weight':weight}, ignore_index=True)
+                        
+            else:
+                productList = brandSoup.find_all("a", {"class": "tile-compare"})
+                for product in productList:
+                    productHtml = requests.get('https://store.midwich.com' + product['href'])
+                    productSoup = bs(productHtml.text,'lxml')
+                    productName = product['href'].split('/')[3]
+                    rrp = productSoup.find('p', {'class':'rrp'}).get_text().strip().replace('RRP:','')
+                    divList = productSoup.find_all('div', {'class':'col-md-7'})
+                    for div in divList:
+                        divC = div.findChildren('strong')
+                        mpn = divC[0].get_text().replace('N/A',"")
+                    try:
+                        description1 = productSoup.find('div', {'class':'midwTruncate'}).findChild('h4').get_text()
+                        description2 = productSoup.find('div', {'class':'midwTruncate'}).findChild('p').get_text()
+                        fullDescription = description1 + '\n' + description2
+                        image = 'https://store.midwich.com' + productSoup.find('img',{'class':'product-image HD1080'})['src']
+                    except:
+                        continue
+
+                    features = productSoup.find_all('td',{'class':'feature'})
+                    for feature in features:
+                        if 'Dimensions' in feature.get_text():
+                            dimensions = feature.find_next('td').text.strip()
+                        if 'Weight' in feature.get_text():
+                            weight = (feature.find_next('td').text.strip().replace('kg',""))
+                    dfMidwich = dfMidwich.append({'Link':'https://store.midwich.com/' + product['href'], 'brand':brandName, 'productName':productName,'RRP':rrp, 'mpn':mpn, 'Description':fullDescription,'image':image, 'dimensions':dimensions,'weight':weight}, ignore_index=True)
+        dfMidwich.to_excel('test1.xlsx',index=False)               
+
+
