ebe6d8e10338c5b72844ef1acc1c2cb2b7cf50b4
Boaz Perlov <boaz@perlov.co.uk>
2022-10-05 16:28:56 +0100

    Almost done with CEF

--- cef/cefScraper.py
+++ cef/cefScraper.py
@@ -15,6 +15,56 @@ dfCef = pd.DataFrame()
 path = "C:/Program Files (x86)/chromedriver.exe"
 driver = webdriver.Chrome(path)
 
+def directProductPage(productLink):
+    mpn = ''
+    sku = ''
+    brand = ''
+    weight = ''
+    warranty = ''
+    description = ''
+    color = ''
+    length = ''
+    diameter = ''
+    height = ''
+    width = ''
+    docsList = []
+    productSoup = bs(requests.get(productLink.get_attribute('href')).text,'lxml')
+    link = productLink.get_attribute('href')
+    try:
+        mpn = productSoup.find('ul',{'class','product-codes'}).findChild('strong').get_text()
+        sku = productSoup.find('ul',{'class','product-codes'}).findChild('li').find_next_sibling().findChild('strong').get_text()          
+    except:
+        pass
+    productName = productSoup.find('h1',{'class','details_page'}).get_text()
+    images = productSoup.find_all('img',{'class':'product-image'})
+    imageList = []
+    featuresList = productSoup.find('ul',{'class','product-features'}).findChildren('li')
+    docs = productSoup.find_all('a',{'class':'download_tracker'})
+    for doc in docs:
+        docsList.append(doc['href'])
+    for feature in featuresList:
+        description += feature.get_text() + '\n'
+    for image in images:
+        imageList.append(image['src'])
+    specTable = productSoup.find('div',{'class','spec-table'}).findChildren('td')
+    for spec in specTable:
+        if 'Brand' in spec.get_text():
+            brand = spec.find_next('td').get_text().strip()
+        if 'Weight' in spec.get_text():
+            weight = spec.find_next('td').get_text().strip()
+        if 'Guarantee' in spec.get_text():
+            warranty = spec.find_next('td').get_text().strip()
+        if 'Length' in spec.get_text():
+            length = spec.find_next('td').get_text().strip()
+        if 'Diameter' in spec.get_text():
+            diameter = spec.find_next('td').get_text().strip()
+        if 'Height' in spec.get_text():
+            height = spec.find_next('td').get_text().strip()
+        if 'Width' in spec.get_text():
+            width = spec.find_next('td').get_text().strip()
+        if spec.get_text().strip() == 'Colour':
+            color = spec.find_next('td').get_text().strip()
+    return {'Link':link, 'brand':brand, 'productName':productName,'mpn':mpn, 'docs':docsList,'description':description, 'Width':width, 'Height':height, 'image':imageList, 'weight':weight, 'sku':sku,'Warranty':warranty, 'Colour':color, 'Diameter':diameter, 'Length':length}
 
 cefSoup = bs(requests.get((originalUrl)).text,'lxml')
 catLists = cefSoup.find_all('a',{'class':'divider-vertical'})
@@ -24,59 +74,42 @@ for catList in tqdm(catLists):
         subCatSoup = bs(requests.get(catUrl).text,'lxml')
         subCatList = subCatSoup.find_all('div',{'class':'top_level'})
         for subCat in subCatList:
-            for j in range(1,10):
-                productPageUrl = subCat.findChild('a')['href'] + '?page='+str(i)
-                driver.set_page_load_timeout(30)
-                driver.get(productPageUrl)
-                productLinks = driver.find_elements(By.CLASS_NAME, 'product-listings__image-wrapper')
-                for productLink in productLinks:
-                    brand = ''
-                    weight = ''
-                    warranty = ''
-                    description = ''
-                    color = ''
-                    length = ''
-                    diameter = ''
-                    productSoup = bs(requests.get(productLink.get_attribute('href')).text,'lxml')
-                    link = productLink.get_attribute('href')
-                    mpn = productSoup.find('ul',{'class','product-codes'}).findChild('strong').get_text()
-                    sku = productSoup.find('ul',{'class','product-codes'}).findChild('li').find_next_sibling().findChild('strong').get_text()          
-                    productName = productSoup.find('h1',{'class','details_page'}).get_text()
-                    images = productSoup.find_all('img',{'class':'product-image'})
-                    imageList = []
-                    featuresList = productSoup.find('ul',{'class','product-features'}).findChildren('li')
-                    for feature in featuresList:
-                        description += feature.get_text() + '\n'
-                    for image in images:
-                        imageList.append(image['src'])
-                        specTable = productSoup.find('div',{'class','spec-table'}).findChildren('td')
-                        for spec in specTable:
-                            if 'Brand' in spec.get_text():
-                                brand = spec.find_next('td').get_text().strip()
-                            if 'Weight' in spec.get_text():
-                                weight = spec.find_next('td').get_text().strip()
-                            if 'Guarantee' in spec.get_text():
-                                warranty = spec.find_next('td').get_text().strip()
-                            if 'Length' in spec.get_text():
-                                length = spec.find_next('td').get_text().strip()
-                            if 'Diameter' in spec.get_text():
-                                diameter = spec.find_next('td').get_text().strip()
-                            if spec.get_text().strip() == 'Colour':
-                                color = spec.find_next('td').get_text().strip()
-                    dfCef = dfCef.append({'Link':link, 'brand':brand, 'productName':productName,'mpn':mpn, 'description':description, 'image':imageList, 'weight':weight, 'sku':sku,'Warranty':warranty, 'Colour':color, 'Diameter':diameter, 'Length':length}, ignore_index=True)
-                dfCef.to_excel('cefScraping.xlsx',index=False) 
+            if 'cctv-fire-security-fire-alarms' in subCat['href']:
+                nextCategorySoup = bs(requests.get(subCat.findChild('a')['href']).text,'lxml')
+                nextCategories = nextCategorySoup.find_all('div', {'class':'product_category_image'})
+                if nextCategories:
+                    for nextCategory in nextCategories:
+                        for j in range(1,10):
+                            productPageUrl = nextCategory.findChild('a')['href'] + '?page='+str(j)
+                            nextNextCategories = bs(requests.get(productPageUrl.text,'lxml'))
+                            if nextNextCategories:
+                                for nextnextCat in nextNextCategories: 
+                                    for j in range(1,10):
+                                        productPageUrl = nextCategory.findChild('a')['href'] + '?page='+str(j)
+                                        driver.set_page_load_timeout(30)
+                                        driver.get(productPageUrl)
+                                        productLinks = driver.find_elements(By.CLASS_NAME, 'product-listings__image-wrapper')
+                                        for productLink in productLinks:
+                                            productDict = directProductPage(productLink)
+                                            dfCef = dfCef.append(productDict, ignore_index=True)
+                            else:
+                                driver.set_page_load_timeout(30)
+                                driver.get(productPageUrl)
+                                productLinks = driver.find_elements(By.CLASS_NAME, 'product-listings__image-wrapper')
+                                for productLink in productLinks:
+                                    productDict = directProductPage(productLink)
+                                    dfCef = dfCef.append(productDict, ignore_index=True)
+                            dfCef.to_excel('cefScraping.xlsx',index=False) 
+                else:
+                    for j in range(1,10):
+                        productPageUrl = subCat.findChild('a')['href'] + '?page='+str(j)
+                        driver.set_page_load_timeout(30)
+                        driver.get(productPageUrl)
+                        productLinks = driver.find_elements(By.CLASS_NAME, 'product-listings__image-wrapper')
+                        for productLink in productLinks:
+                            productDict = directProductPage(productLink)
+                            dfCef = dfCef.append(productDict, ignore_index=True)
+                        dfCef.to_excel('cefScraping.xlsx',index=False) 
+
 dfCef.to_excel('cefScraping.xlsx',index=False) 
 
-
-
-
-
-
-
-
-
-'''productUrl = subCat.findChild('a')['href']
-productHtml = requests.get(productUrl)
-productSoup = bs(productHtml.text,'lxml')
-productLink = productSoup.find_all('ul', {'class':'product-listings__codes'})
-print(productLink)'''
--- cef/rrpfunction.py
+++ cef/rrpfunction.py
@@ -19,7 +19,15 @@ def rrpFunction(prodUrl):
     rrp = driver.find_element(By.CLASS_NAME,'main-value')
     return rrp.text
 
+def findCategory(prodUrl):
+    catList = []
+    driver.set_page_load_timeout(30) 
+    driver.get(prodUrl)
+    catList = driver.find_elements(By.itemprop,'name')
+    return catList
+
 for index, row in dfCef.iterrows():
     dfCef.loc[index,'rrp'] = rrpFunction(dfCef.loc[index,'Link'])
+    dfCef.loc[index,'CEF Category'] = findCategory(dfCef.loc[index,'Link'])
 
 dfCef.to_excel('cefScraping v2.xlsx', index=False)
